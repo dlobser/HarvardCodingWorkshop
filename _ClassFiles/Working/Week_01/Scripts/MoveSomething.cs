18e4abc456864e7951381bb74d4bb3f887295c97
david lobser <davidlobser@yahoo.com>
2021-02-15 18:08:43 -0700

    added class 1 and 2

---
+++ _ClassFiles/Working/Week_01/Scripts/MoveSomething.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSomething : MonoBehaviour
+{
+
+    //int
+    //float
+    //bool
+    //string
+
+    //GameObject
+    //Transform
+    //Vector3
+
+    public string Hello = "Hello World!";
+    public int intVariable;
+    public float floatVariable;
+    public bool boolVariable;
+
+    public float x;
+    public float y;
+    public float z;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(Hello + " , " + intVariable + " , " + floatVariable + " , " + boolVariable);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(Hello + " , " + intVariable + " , " + floatVariable + " , " + boolVariable);
+        x += speed;
+        this.transform.position = new Vector3(x, y, z);
+    }
+}
---
+++ _ClassFiles/Working/Week_02/Scripts/AnimateBird.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class AnimateBird : MonoBehaviour
+{
+    public float speed;
+    public float offset;
+    public float multiply;
+
+    public float neckMultiply;
+    public float neckOffset;
+
+    public float bodyMultiply;
+    public float bodyOffset;
+
+    float wingRotation;
+    float wingRotation2;
+    float neckRotation;
+    float bodyPosition;
+
+    public Transform rWing;
+    public Transform lWing;
+    public Transform neck;
+    public Transform body;
+
+    float LFO;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        LFO = Mathf.Sin(Time.time * speed * .1f);
+
+        wingRotation = Mathf.Sin(Time.time * speed) * multiply * LFO;
+        wingRotation2 = Mathf.Sin(Time.time * speed + offset) * multiply * LFO;
+        neckRotation = Mathf.Sin(Time.time * speed + neckOffset) * neckMultiply * LFO;
+        bodyPosition = Mathf.Sin(Time.time * speed + bodyOffset) * bodyMultiply * LFO;
+
+
+        rWing.localEulerAngles = new Vector3(0, 0, wingRotation );
+        rWing.GetChild(0).localEulerAngles = new Vector3(0, 0, wingRotation2);
+
+        lWing.localEulerAngles = new Vector3(0, 0, wingRotation);
+        lWing.GetChild(0).localEulerAngles = new Vector3(0, 0, wingRotation2);
+
+        neck.localEulerAngles = new Vector3(neckRotation, 0 , 0);
+        body.localPosition = new Vector3(0, bodyPosition, 0);
+
+    }
+}
