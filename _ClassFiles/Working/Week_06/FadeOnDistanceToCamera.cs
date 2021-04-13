836ee68631665eab2c759857504153e8fb439379
david lobser <davidlobser@yahoo.com>
2021-04-13 08:08:31 -0600

    week 6

---
+++ _ClassFiles/Working/Week_06/FadeOnDistanceToCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOnDistanceToCamera : MonoBehaviour
+{
+
+    public SpriteRenderer spriteRenderer;
+    public float value;
+    public float min = -1;
+    public float max = 1;
+    public bool isTransparent;
+
+    void Start()
+    {
+        if(spriteRenderer==null)
+            spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (!isTransparent)
+        {
+            float dist = Vector3.Distance(Camera.main.transform.position, this.transform.position);
+            float remappedDist = map(dist, 0, 1, min, max);
+            spriteRenderer.color = new Color(1, 1, 1, remappedDist);
+            if (remappedDist <= 0)
+            {
+                isTransparent = true;
+            }
+        }
+    }
+
+    float map(float s, float a1, float a2, float b1, float b2)
+    {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+    }
+}
---
+++ _ClassFiles/Working/Week_06/TurnOnSprites.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnSprites : MonoBehaviour
+{
+    public SpriteRenderer[] sprites;
+
+    public float fadeSpeed;
+    public float gapSpeed;
+
+    public float fadeCounter;
+    public float gapCounter;
+
+    public int whichSprite;
+
+    public Color color = Color.white;
+
+    public bool trigger;
+
+
+    //turn all the sprites off
+    //count up a float by the fade speed
+    //count up a float by the gap speed
+    //iterate up the index of the sprite when the fade speed == 1
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        color.a = 0;
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            sprites[i].color = color;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger)
+        {
+            if (fadeCounter < 1)
+            {
+                fadeCounter += Time.deltaTime * (1 / fadeSpeed);
+            }
+            else if (gapCounter < 1)
+            {
+                gapCounter += Time.deltaTime * (1 / gapSpeed);
+            }
+            else
+            {
+                if (whichSprite + 1 < sprites.Length)
+                {
+                    whichSprite++;
+                    fadeCounter = 0;
+                    gapCounter = 0;
+
+                }
+            }
+
+            color.a = fadeCounter;
+            sprites[whichSprite].color = color;
+        }
+    }
+}
